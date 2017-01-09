ba70e2c142d4673b063f1944189bf0f1229bdfb8
Brandon Risell <brisell@frontporch.com>
2017-01-09 08:29:18 -0800

    Update root page and add second page to test navigation threads stepping on one another

---
+++ NavigationPop/NavigationPop.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace NavigationPop
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var nextPageButton = new Button
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                Text = "Next Page"
+            };
+
+            nextPageButton.Clicked += OnNextPageButtonClicked;
+
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "NavigationPop",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Root page. Click below."
+                        },
+                        nextPageButton
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        private void OnNextPageButtonClicked(object sender, EventArgs e)
+        {
+            MainPage.Navigation.PushAsync(new Page2());
+        }
+    }
+}
---
+++ NavigationPop/Page2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigationPop
+{
+    public class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            var popPageButton = new Button
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                Text = "Pop This Page"
+            };
+
+            popPageButton.Clicked += OnPopPageButtonClicked;
+
+            Title = "Page 2";
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    popPageButton
+                }
+            };
+        }
+
+        private void OnPopPageButtonClicked(object sender, EventArgs e)
+        {
+            //Task.Run(async () => await Navigation.PopAsync());
+            //Task.Run(async () => await Navigation.PopAsync());
+            //Task.Run(async () => await Navigation.PopAsync());
+            //Task.Run(() => Navigation.PopAsync());
+            Navigation.PopAsync();
+            Navigation.PopAsync();
+            Navigation.PopAsync();
+        }
+    }
+}
